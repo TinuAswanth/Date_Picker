8adf7febc42b956eec713562d893a1f3a7ef025f
Tinu AswanthL <tinuaswanth13@gmail.com>
2023-07-07 14:02:46 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  DateTime selectedDate = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "${selectedDate.year} - ${selectedDate.month} - ${selectedDate.day}",
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                final DateTime? dateTime = await showDatePicker(
+                  context: context,
+                  initialDate: selectedDate,
+                  firstDate: DateTime(2000),
+                  lastDate: DateTime.now(),
+                );
+                if (dateTime != null) {
+                  setState(() {
+                    selectedDate = dateTime;
+                  });
+                }
+              },
+              child: const Text("Choose Date"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
